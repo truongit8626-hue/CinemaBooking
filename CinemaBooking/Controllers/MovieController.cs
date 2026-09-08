cfffd6945fd939d38d2a7694342d85bc0fe54436
Xuan Truong <Xuan Truong@LAPTOP-CF1M6K18>
2026-09-08 17:14:57 +0700

    Add project files.

---
+++ CinemaBooking/Controllers/MovieController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CinemaBooking.Data;
+using CinemaBooking.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace CinemaBooking.Controllers
+{
+    [Authorize]
+    public class MovieController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MovieController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Movies.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Movie movie)
+        {
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult GetMovie(int id)
+        {
+            var movie = _context.Movies.FirstOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return NotFound();
+
+            return Json(movie);
+        }
+        public IActionResult Details(int id)
+        {
+            var movie = _context.Movies.FirstOrDefault(m => m.Id == id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+    }
+
+}
---
+++ CinemaBooking/Data/ApplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using CinemaBooking.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace CinemaBooking.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Showtime> Showtimes { get; set; }
+        public DbSet<Seat> Seats { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<BookingDetail> BookingDetails { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        
+
+        // 🔥 THÊM ĐOẠN NÀY
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Seat>()
+            .HasOne(s => s.Showtime)
+            .WithMany()
+            .HasForeignKey(s => s.ShowtimeId)
+            .OnDelete(DeleteBehavior.Restrict);
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Showtime>()
+                .HasOne(s => s.Room)
+                .WithMany(r => r.Showtimes)
+                .HasForeignKey(s => s.RoomId);
+
+            modelBuilder.Entity<Showtime>()
+                .HasOne(s => s.Movie)
+                .WithMany()
+                .HasForeignKey(s => s.MovieId);
+        }
+        
+    }
+}
